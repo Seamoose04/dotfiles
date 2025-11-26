9e4234b570bd7a3a4dedcd44c2b6d197869ad0e4
seamoose04 <seamusjohnson04@gmail.com>
2025-11-26 20:45:26 +0000

    Removed irrelavent lines

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,8 +18,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true }) -- Ha
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true }) -- Half-page up + recenter
 
 -- Move selections with ctrl
--- vim.keymap.set("v", "<C-k>", "d2kp'[V']=gv", { noremap = true, silent = true }) -- Selection Up
--- vim.keymap.set("v", "<C-j>", "dp'[V']=gv", { noremap = true, silent = true }) -- Selection Down
 vim.keymap.set("n", "<C-k>", "dd2kp==", { noremap = true, silent = true }) -- Line Up
 vim.keymap.set("n", "<C-j>", "ddp==", { noremap = true, silent = true }) -- Line Down
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>'[V']=gv", { noremap = true, silent = true }) -- Selection Up
