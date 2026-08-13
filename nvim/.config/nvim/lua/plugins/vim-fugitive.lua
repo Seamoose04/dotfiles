eb241f9d2fb19b5e949789d54d75bde1a58c2b59
seamoose04 <seamusjohnson04@gmail.com>
2026-08-12 22:26:06 -0400

    Added git branch command to nvim

--- nvim/.config/nvim/lua/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/vim-fugitive.lua
@@ -6,12 +6,25 @@ return {
 		{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
 		{ "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
 		{ "<leader>gu", "<cmd>Git pull<cr>", desc = "Git pull" },
-		{ "<leader>gi",
-		function()
-			vim.fn.system("git init")
-			vim.notify("Initialized new git repository", vim.log.levels.INFO)
-		end,
+		{
+			"<leader>gi",
+			function()
+				vim.fn.system("git init")
+				vim.notify("Initialized new git repository", vim.log.levels.INFO)
+			end,
 			desc = "Git init"
 		},
+		{
+			"<leader>gb",
+			function()
+				vim.ui.input({ prompt = "Branch name: " }, function(branch)
+					if not branch or branch == "" then
+						return
+					end
+					vim.cmd("Git checkout -b " .. branch .. " || Git checkout " .. branch)
+				end)
+			end,
+			desc = "Git checkout/create branch"
+		}
 	}
 }
