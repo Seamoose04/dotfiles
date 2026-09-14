8787ab3c47a2283d83d3a21d790b53e398c71b0a
seamoose04 <seamusjohnson04@gmail.com>
2026-09-14 09:20:59 -0400

    Added propper root marker

--- nvim/.config/nvim/lsp/clangd.lua
+++ nvim/.config/nvim/lsp/clangd.lua
@@ -1,6 +1,10 @@
 return {
-    cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/clangd"), "--background-index", "--clang-tidy" },
+    cmd = {
+		vim.fn.expand("~/.local/share/nvim/mason/bin/clangd"),
+		"--background-index",
+		"--query-driver=" .. vim.fn.expand("~/.espressif/tools/xtensa-esp-elf/esp-13.2.0_20240530/bin/xtensa-esp32s3-elf-gcc")
+	},
     filetypes = { "c", "cpp" },
-    root_markers = { "compile_commands.json", ".git" },
+    root_markers = { ".clangd", "compile_commands.json", ".git" },
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
 }
